4ac059f9a806a6d8a3678d62900f7286b6d42627
Karma-234 <busari1kamal@gmail.com>
2023-10-31 11:06:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ai_object_tracker
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ai_object_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ai_object_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
